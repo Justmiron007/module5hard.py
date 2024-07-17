f509e1f687bc9b27e6a11fe11f9659f7f5c56111
Justmiron007 <raen.96@mail.ru>
2024-07-17 23:47:53 +0500

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -7,12 +7,12 @@ class UrTube:
         self.videos = []
         self.current_user = None
 
-    def log_in(self, login: str, password: str) -> None:
+    def log_in(self, login, password):
         for user in self.users:
             if login == user.nickname and password == user.password:
                 self.current_user = user
 
-    def register(self, nickname: str, password: str, age: int) -> None:
+    def register(self, nickname, password, age):
         for user in self.users:
             if nickname in user.nickname:
                 print(f"Пользователь {nickname} уже существует")
@@ -20,8 +20,7 @@ class UrTube:
         else:
             user = User(nickname, password, age)
             self.users.append(user)
-            self.log_out()
-            self.log_in(user.nickname, user.password)
+            self.current_user = user
 
     def log_out(self):
         self.current_user = None
@@ -37,7 +36,7 @@ class UrTube:
                 list_movie.append(video.title)
         return list_movie
 
-    def watch_video(self, movie: str) -> None:
+    def watch_video(self, movie):
         if self.current_user and self.current_user.age < 18:
             print('Вам нет 18 лет, пожалуйста покиньте страницу')
         elif self.current_user:
